e8a8cddc8bc132b965d5fa1460ac3e91c7ddbf6d
taek2222 <oot309@daum.net>
2024-11-07 21:39:23 +0900

    test(ProductTest): 물품 테스트 리팩토링
    - BigInteger 변경으로 매개변수 전달 값을 전체 변경했습니다.
    
    - 예외 발생 로직을 간소화했습니다.
    
    - 여러 파라미터 테스트를 위해 반복해 확인하는 방법으로 변경했습니다.
    
    - 에러 메시지를 추가해 예외시 알맞는 처리가 되었는지 추가로 정의했습니다.

--- src/test/java/store/domain/ProductTest.java
+++ src/test/java/store/domain/ProductTest.java
@@ -2,8 +2,14 @@ package store.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static store.constant.ErrorMessage.INVALID_NAME_BLANK;
+import static store.constant.ErrorMessage.INVALID_NAME_NOT_NULL;
+import static store.constant.ErrorMessage.INVALID_PRICE_TOO_LOW;
 
+import java.math.BigInteger;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ProductTest {
 
@@ -11,79 +17,56 @@ public class ProductTest {
     void 이름과_가격으로_물품을_등록한다() {
         // given
         String name = "콜라";
-        int price = 1_000;
+        BigInteger price = BigInteger.valueOf(1_000);
 
-        // when
-        Product product = new Product(name, price);
-
-        // then
-        assertThat(product)
+        // when & then
+        assertThat(new Product(name, price))
                 .isNotNull();
     }
 
     @Test
     void 구매_수량의_물품_총가격을_반환한다() {
         // given
-        String name = "콜라";
-        int price = 1_000;
+        Product product = new Product("콜라", BigInteger.valueOf(1_000));
         int quantity = 5;
-        Product product = new Product(name, price);
 
         // when
-        int totalPrice = product.calculateTotalPrice(quantity);
+        int totalPrice = product.calculateTotalPrice(quantity).intValue();
 
         // then
-        assertThat(totalPrice).isEqualTo(5_000);
+        assertThat(totalPrice)
+                .isEqualTo(5_000);
     }
 
     @Test
     void 등록_이름이_NULL_일_경우_예외가_발생한다() {
-        assertThatThrownBy(() -> new Product(null, 1_000))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Product(null, BigInteger.ONE))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_NAME_NOT_NULL.getMessage());
     }
 
-    @Test
-    void 등록_이름이_공백일_경우_예외가_발생한다() {
-        // given
-        String name = " ";
-        int price = 1_000;
-
-        // when & then
-        assertThatThrownBy(() -> new Product(name, price))
-                .isInstanceOf(IllegalArgumentException.class);
+    @ParameterizedTest(name = "테스트 이름 = [{arguments}]")
+    @ValueSource(strings = {" ", ""})
+    void 등록_이름이_공백일_경우_예외가_발생한다(String name) {
+        assertThatThrownBy(() -> new Product(name, BigInteger.ONE))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_NAME_BLANK.getMessage());
     }
 
-    @Test
-    void 등록_가격이_1원보다_작을_경우_예외가_발생한다() {
-        // given
-        String name = "콜라";
-        int price = 0;
+    @ParameterizedTest(name = "테스트 가격 = [{arguments}]")
+    @ValueSource(ints = {1, 1000})
+    void 등록_가격이_최소보다_같거나_큰_경우_객체가_생성된다(int price) {
+        BigInteger value = BigInteger.valueOf(price);
+        assertThat(new Product("콜라", value))
+                .isNotNull();
+    }
 
-        // when & then
-        assertThatThrownBy(() -> new Product(name, price))
-                .isInstanceOf(IllegalArgumentException.class);
+    @ParameterizedTest(name = "테스트 가격 = [{arguments}]")
+    @ValueSource(ints = {-1000, 0})
+    void 등록_가격이_최소보다_작을_경우_예외가_발생한다(int price) {
+        BigInteger value = BigInteger.valueOf(price);
+        assertThatThrownBy(() -> new Product("콜라", value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_PRICE_TOO_LOW.getMessage());
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
