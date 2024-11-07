aa6bc4a3af7b4f38e61b2ed71786368cbe0a143a
taek2222 <oot309@daum.net>
2024-11-07 18:52:28 +0900

    feat(Product): 이름과 가격으로 물품 객체 정의
    - 물품을 보관하는 객체를 정의하고, 가리키는 이름과 가격을 필드로 선언했습니다.

---
+++ src/main/java/store/domain/Product.java
@@ -0,0 +1,11 @@
+package store.domain;
+
+public class Product {
+    private String name;
+    private Integer price;
+
+    public Product(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
+}
