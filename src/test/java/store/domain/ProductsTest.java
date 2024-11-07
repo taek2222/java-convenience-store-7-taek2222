c378329f910531a6e3ed1eb2f430c3df8b9dacfa
taek2222 <oot309@daum.net>
2024-11-08 02:31:06 +0900

    test(ProductsTest): 전체 상품 정보 반환 테스트
    - 성공적으로 리스트에 반환되는지 테스트를 설계했습니다.

---
+++ src/test/java/store/domain/ProductsTest.java
@@ -0,0 +1,25 @@
+package store.domain;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProductsTest {
+
+    @Test
+    void 전체_상품의_정보를_반환한다() {
+        // given
+        Products products = new Products();
+        products.addProduct(new Product("콜라", "2000", "5", "테스트할인1"));
+        products.addProduct(new Product("사이다", "1500", "2", "테스트할인2"));
+
+        // when
+        List<List<String>> allProductsInfo = products.getAllProductsInfo();
+
+        // then
+        Assertions.assertThat(allProductsInfo).containsExactly(
+                List.of("콜라", "2,000", "5", "테스트할인1"),
+                List.of("사이다", "1,500", "2", "테스트할인2")
+        );
+    }
+}
