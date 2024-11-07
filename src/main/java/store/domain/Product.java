62beaf0afd7212ef189c1f2a3ccbe3883d752e9a
taek2222 <oot309@daum.net>
2024-11-07 19:03:47 +0900

    feat(Product): 등록 가격이 1원보다 작을 경우 예외 추가
    - 물품을 등록할 때, 가격이 1원보다 작을 경우 예외를 발생시켰습니다.

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -4,14 +4,22 @@ public class Product {
     private String name;
     private Integer price;
 
-    public Product(String name, Integer price) {
+    public Product(String name, int price) {
         validateName(name);
+        validatePrice(price);
         this.name = name;
         this.price = price;
     }
 
     private void validateName(String name) {
-        if (name.isBlank())
+        if (name.isBlank()) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validatePrice(int price) {
+        if (price < 1) {
+            throw new IllegalArgumentException();
+        }
     }
 }
