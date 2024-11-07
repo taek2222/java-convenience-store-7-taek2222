23355ba7fdaf2001adc7f09a79fd096c01bc33ba
taek2222 <oot309@daum.net>
2024-11-07 20:35:08 +0900

    refactor(Product): 상수 분리 및 int 형 변경
    - 상수와 매직 넘버를 상수로 분리해 변경했습니다.
    
    - 에러 메시지를 정의하고, 메시지를 설정했습니다.
    
    - int 형의 범위 제한과 더 많은 메소드 활용을 위해 BigInteger 로 변경했습니다.

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -1,21 +1,28 @@
 package store.domain;
 
-public class Product {
-    private String name;
-    private Integer price;
+import java.math.BigInteger;
 
-    public Product(String name, int price) {
-        validateName(name);
-        validatePrice(price);
-        this.name = name;
+public class Product {
+    public static final String INVALID_NAME_NOT_BLANK = "물품 이름은 공백 및 빈칸일 수 없습니다.";
+    public static final String INVALID_PRICE_MIN = "최소 금액(1원) 이상의 물품만 등록할 수 있습니다.";
+    public static final String INVALID_NAME_NOT_NULL = "물품 이름은 NULL 일 수 없습니다.";
+
+    private static final BigInteger MIN_PRICE = BigInteger.ONE;
+
+    private final String name;
+    private final BigInteger price;
+
+    public Product(final String name, final BigInteger price) {
+        validate(name, price);
+        this.name = name.trim(); // 상품 이름 앞뒤 공백 제거
         this.price = price;
     }
 
-    public int calculateTotalPrice(int quantity) {
-        return quantity * price;
+    public BigInteger calculateTotalPrice(final int quantity) {
+        return price.multiply(BigInteger.valueOf(quantity));
     }
 
-    private void validate(String name, int price) {
+    private void validate(final String name, final BigInteger price) {
         validateNull(name);
         validateName(name);
         validatePrice(price);
@@ -29,13 +36,13 @@ public class Product {
 
     private void validateName(final String name) {
         if (name.isBlank()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INVALID_NAME_NOT_BLANK);
         }
     }
 
-    private void validatePrice(int price) {
-        if (price < 1) {
-            throw new IllegalArgumentException();
+    private void validatePrice(final BigInteger price) {
+        if (price.compareTo(MIN_PRICE) < 0) {
+            throw new IllegalArgumentException(INVALID_PRICE_MIN);
         }
     }
 }
