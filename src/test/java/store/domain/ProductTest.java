d6a6e9808ac27bbd9a1401aa3341396420825936
taek2222 <oot309@daum.net>
2024-11-07 19:16:33 +0900

    test(ProductTest): 구매 수량의 물품 총 가격 반환으로 변경
    - 구체적인 메시지를 고려해, 총 가격 반환으로 변경했습니다.

--- src/test/java/store/domain/ProductTest.java
+++ src/test/java/store/domain/ProductTest.java
@@ -1,6 +1,8 @@
 package store.domain;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.api.Test;
 
 public class ProductTest {
@@ -9,48 +11,50 @@ public class ProductTest {
     void 이름과_가격으로_물품을_등록한다() {
         // given
         String name = "콜라";
-        Integer price = 1_000;
+        int price = 1_000;
 
         // when
         Product product = new Product(name, price);
 
         // then
-        Assertions.assertThat(product)
+        assertThat(product)
                 .isNotNull();
     }
 
     @Test
-    void 이름으로_물품_가격을_반환한다() {
+    void 구매_수량의_물품_총가격을_반환한다() {
         // given
-        String searchName = "콜라";
-        Product product = new Product("콜라", 1_000);
+        String name = "콜라";
+        int price = 1_000;
+        int quantity = 5;
+        Product product = new Product(name, price);
 
         // when
-        int price = product.getPriceByName(searchName);
+        int totalPrice = product.calculateTotalPrice(quantity);
 
         // then
-        Assertions.assertThat(price).isEqualTo(1_000);
+        assertThat(totalPrice).isEqualTo(5_000);
     }
 
     @Test
     void 등록_이름이_공백일_경우_예외가_발생한다() {
         // given
         String name = " ";
-        Integer price = 1_000;
+        int price = 1_000;
 
         // when & then
-        Assertions.assertThatThrownBy(new Product(name, price))
+        assertThatThrownBy(() -> new Product(name, price))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 등록_가격이_0원보다_작을_경우_예외가_발생한다() {
+    void 등록_가격이_1원보다_작을_경우_예외가_발생한다() {
         // given
         String name = "콜라";
-        Integer price = 0;
+        int price = 0;
 
         // when & then
-        Assertions.assertThatThrownBy(new Product(name, price))
+        assertThatThrownBy(() -> new Product(name, price))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
