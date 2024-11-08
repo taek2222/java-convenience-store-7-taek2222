8b6c9f8cf89057eca203d6092b1ea2d8adcc4045
taek2222 <oot309@daum.net>
2024-11-09 03:22:14 +0900

    test(ProductTest): 상품 정보 리스트 크기 테스트
    - 발생 시 출력되는 에러 메시지도 동시에 정의했습니다.

--- src/test/java/store/domain/ProductTest.java
+++ src/test/java/store/domain/ProductTest.java
@@ -1,6 +1,8 @@
 package store.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static store.constant.ErrorMessage.INVALID_PRODUCT_INFO;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -28,5 +30,16 @@ public class ProductTest {
         // then
         assertThat(info)
                 .containsExactly("콜라", "2,000", "5", "테스트할인");
+    }
+
+    @Test
+    void 상품_정보_리스트_크기가_4가_아닌_경우_예외가_발생한다() {
+        // given
+        List<String> product = List.of("콜라", "2000", "5");
+
+        // when & then
+        assertThatThrownBy(() -> new Product(product))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_PRODUCT_INFO.getMessage());
     }
 }
