267f135058cc6937d00a0071f9ffc6b4645e74c2
taek2222 <oot309@daum.net>
2024-11-09 03:11:35 +0900

    test(PriceTest): 에러 메시지 테스트 및 파라미터 변경
    - 실수로 기존 파라미터 타입을 잘못 설정해 변경했습니다.

--- src/test/java/store/domain/product/PriceTest.java
+++ src/test/java/store/domain/product/PriceTest.java
@@ -1,5 +1,8 @@
 package store.domain.product;
 
+import static store.constant.ErrorMessage.INVALID_PRICE_NUMERIC;
+import static store.constant.ErrorMessage.INVALID_PRICE_OUT_OF_RANGE;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -21,22 +24,25 @@ public class PriceTest {
     }
 
     @ParameterizedTest(name = "테스트 가격 : [{arguments}]")
-    @ValueSource(ints = {0, -1000})
-    void 등록_가격이_최소보다_작을_경우_예외가_발생한다(int price) {
+    @ValueSource(strings = {"0", "-1000"})
+    void 등록_가격이_최소보다_작을_경우_예외가_발생한다(String price) {
         Assertions.assertThatThrownBy(() -> new Price(price))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_PRICE_OUT_OF_RANGE.getMessage());
     }
 
     @ParameterizedTest(name = "테스트 가격 : [{arguments}]")
-    @ValueSource(ints = {1_000_000, 1_001_000})
-    void 등록_가격이_최대보다_클_경우_예외가_발생한다(int price) {
+    @ValueSource(strings = {"1000001", "1001000"})
+    void 등록_가격이_최대보다_클_경우_예외가_발생한다(String price) {
         Assertions.assertThatThrownBy(() -> new Price(price))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_PRICE_OUT_OF_RANGE.getMessage());
     }
 
     @Test
     void 등록_가격이_숫자가_아닌_경우_예외가_발생한다() {
         Assertions.assertThatThrownBy(() -> new Price("가격"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_PRICE_NUMERIC.getMessage());
     }
 }
