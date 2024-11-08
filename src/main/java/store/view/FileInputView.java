bb5c4f9b8605a6ba35fcaabd83e0507714d58797
taek2222 <oot309@daum.net>
2024-11-08 17:33:34 +0900

    refactor(FileInputView): 로직 간편화 및 가독성 향상
    - 가독성 향상을 위해 Stream API로 로직을 간소화했습니다.
    
    - 변수와 메소드 네이밍을 변경해 명확한 이름을 가지도록 했습니다.

--- src/main/java/store/view/FileInputView.java
+++ src/main/java/store/view/FileInputView.java
@@ -4,17 +4,17 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FileInputView {
 
     public List<List<String>> readFile(String fileName) {
-        BufferedReader fileContents = loadFileContents(fileName);
-        return parseFileContents(fileContents);
+        BufferedReader fileReader = openFileReader(fileName);
+        return parseLines(fileReader);
     }
 
-    private BufferedReader loadFileContents(String fileName) {
+    private BufferedReader openFileReader(String fileName) {
         try {
             File file = new File("src/main/resources/" + fileName);
             FileReader fileReader = new FileReader(file);
@@ -24,15 +24,14 @@ public class FileInputView {
         }
     }
 
-    private List<List<String>> parseFileContents(BufferedReader fileContents) {
-        List<List<String>> listList = new ArrayList<>();
+    private List<List<String>> parseLines(BufferedReader fileReader) {
+        return fileReader.lines()
+                .map(this::parseLine)
+                .toList();
+    }
 
-        fileContents.lines().forEach(line -> {
-            String[] split = line.split(",");
-            List<String> list = List.of(split);
-            listList.add(list);
-        });
-
-        return listList;
+    private List<String> parseLine(String line) {
+        return Arrays.stream(line.split(","))
+                .toList();
     }
 }
