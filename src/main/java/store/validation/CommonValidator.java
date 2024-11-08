1ead260858076817fe846123e3a64839cd96b787
taek2222 <oot309@daum.net>
2024-11-09 03:08:49 +0900

    feat(CommonValidator): 숫자 변환 검증 추가
    - 변환할 수 없는 입력일 경우 에러를 발생시키는 검증을 추가했습니다.

--- src/main/java/store/validation/CommonValidator.java
+++ src/main/java/store/validation/CommonValidator.java
@@ -4,14 +4,22 @@ import store.constant.ErrorMessage;
 
 public class CommonValidator {
 
-    public static void validateNull(final String input, ErrorMessage error) {
+    public static void validateNull(final String input, final ErrorMessage error) {
         if (input == null) {
             throw new IllegalArgumentException(error.getMessage());
         }
     }
 
-    public static void validateBlank(final String input, ErrorMessage error) {
+    public static void validateBlank(final String input, final ErrorMessage error) {
         if (input.isBlank()) {
+            throw new IllegalArgumentException(error.getMessage());
+        }
+    }
+
+    public static void validateNotNumeric(final String input, final ErrorMessage error) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(error.getMessage());
         }
     }
