42af1211c895f8b4e2c62771d73de6baf7284ad9
taek2222 <oot309@daum.net>
2024-11-09 01:12:30 +0900

    feat(Products): 전체 상품 등록 기능 구현
    - 파일 입력으로 이루어진, 전체 상품 정보 리스트로 여러 상품을 등록하는 기능을 구현했습니다.

--- src/main/java/store/domain/Products.java
+++ src/main/java/store/domain/Products.java
@@ -1,22 +1,23 @@
 package store.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Products {
     List<Product> products;
 
-    public Products() {
-        this.products = new ArrayList<>();
-    }
-
-    public void addProduct(Product product) {
-        products.add(product);
+    public Products(List<List<String>> products) {
+        this.products = createProducts(products);
     }
 
     public List<List<String>> getAllProductsInfo() {
         return products.stream()
                 .map(Product::buildInfo)
+                .toList();
+    }
+
+    private List<Product> createProducts(List<List<String>> products) {
+        return products.stream()
+                .map(Product::new)
                 .toList();
     }
 }
