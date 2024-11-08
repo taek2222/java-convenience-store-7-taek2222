a3ba36f7b5db2f88c2220efaf3c3b396455320c4
taek2222 <oot309@daum.net>
2024-11-08 22:49:48 +0900

    test(ProductTest): 상품 등록 테스트
    - 파일로 파싱한 리스트로 상품을 만드는 테스트를 설계했습니다.

--- src/test/java/store/domain/ProductTest.java
+++ src/test/java/store/domain/ProductTest.java
@@ -1,10 +1,21 @@
 package store.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ProductTest {
+
+    @Test
+    void 상품_정보_리스트로_상품을_등록한다() {
+        // given
+        List<String> product = List.of("콜라", "2000", "5", "테스트할인");
+
+        // when & then
+        assertThat(new Product(product))
+                .isNotNull();
+    }
 
     @Test
     void 단일_상품의_정보를_반환한다() {
@@ -15,7 +26,7 @@ public class ProductTest {
         List<String> info = product.buildInfo();
 
         // then
-        Assertions.assertThat(info)
+        assertThat(info)
                 .containsExactly("콜라", "2,000", "5", "테스트할인");
     }
 }
