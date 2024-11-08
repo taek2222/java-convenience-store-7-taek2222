c312d3566c0721142107a514788d94d6279fdb9d
taek2222 <oot309@daum.net>
2024-11-09 02:56:18 +0900

    feat(Price): 가격 예외 검증 추가
    - 숫자로 변경이 가능한지, 범위안에 드는 숫자인지 검증하는 로직을 추가했습니다.

--- src/main/java/store/domain/product/Price.java
+++ src/main/java/store/domain/product/Price.java
@@ -1,20 +1,30 @@
 package store.domain.product;
 
-import java.math.BigInteger;
+import static store.constant.ErrorMessage.INVALID_PRICE_NUMERIC;
+import static store.validation.CommonValidator.validateNotNumeric;
+
 import java.text.DecimalFormat;
 
 public class Price {
 
     private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("###,###");
 
-    private final BigInteger price;
+    private final int price;
 
-    public Price(final String price) {
-        this.price = new BigInteger(price);
+    public Price(final String inputPrice) {
+        validateNotNumeric(inputPrice, INVALID_PRICE_NUMERIC);
+        int price = Integer.parseInt(inputPrice);
+        validateRange(price);
+        this.price = price;
     }
 
     @Override
     public String toString() {
         return DECIMAL_FORMAT.format(price);
+    }
+
+    private void validateRange(final int price) {
+        if (price < 1 || price > 1_000_000)
+            throw new IllegalArgumentException();
     }
 }
