d5bb9b6cb108b48d7b65cc50bec2c94f1769a342
taek2222 <oot309@daum.net>
2024-11-09 01:33:11 +0900

    test(PriceTest): 가격 예외 처리 테스트
    - 가격 객체 생성 과정에서 처리하는 예외를 테스트 하기 위해 작성했습니다.
    
    - 숫자가 아닌 예외의 경우를 추가로 정의했습니다.

--- src/test/java/store/domain/product/PriceTest.java
+++ src/test/java/store/domain/product/PriceTest.java
@@ -2,6 +2,8 @@ package store.domain.product;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class PriceTest {
 
@@ -16,5 +18,25 @@ public class PriceTest {
         // then
         Assertions.assertThat(toString)
                 .isEqualTo("2,000");
+    }
+
+    @ParameterizedTest(name = "테스트 가격 : [{arguments}]")
+    @ValueSource(ints = {0, -1000})
+    void 등록_가격이_최소보다_작을_경우_예외가_발생한다(int price) {
+        Assertions.assertThatThrownBy(() -> new Price(price))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest(name = "테스트 가격 : [{arguments}]")
+    @ValueSource(ints = {1_000_000, 1_001_000})
+    void 등록_가격이_최대보다_클_경우_예외가_발생한다(int price) {
+        Assertions.assertThatThrownBy(() -> new Price(price))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 등록_가격이_숫자가_아닌_경우_예외가_발생한다() {
+        Assertions.assertThatThrownBy(() -> new Price("가격"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
