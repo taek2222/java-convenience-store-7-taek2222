d6675355f3c44c0bfaf3ebf6c813b94a9bef78ea
taek2222 <oot309@daum.net>
2024-11-09 01:21:00 +0900

    feat(Name): 상품 이름 검증 추가
    - 이름이 Null 또는 공백일 경우 예외처리할 수 있도록 검증을 추가했습니다.
    
    - 기존 공통 검증 로직을 재사용했습니다.

--- src/main/java/store/domain/product/Name.java
+++ src/main/java/store/domain/product/Name.java
@@ -1,15 +1,26 @@
 package store.domain.product;
 
+import static store.constant.ErrorMessage.INVALID_NAME_BLANK;
+import static store.constant.ErrorMessage.INVALID_NAME_NOT_NULL;
+import static store.validation.CommonValidator.validateBlank;
+import static store.validation.CommonValidator.validateNull;
+
 public class Name {
 
     private final String name;
 
     public Name(final String name) {
+        validate(name);
         this.name = name;
     }
 
     @Override
     public String toString() {
         return name;
+    }
+
+    private void validate(String name) {
+        validateNull(name, INVALID_NAME_NOT_NULL);
+        validateBlank(name, INVALID_NAME_BLANK);
     }
 }
