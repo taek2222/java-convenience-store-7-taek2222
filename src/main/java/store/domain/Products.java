0c39e006cd4d04379b655e7f506e15a80b69ff71
taek2222 <oot309@daum.net>
2024-11-10 05:56:41 +0900

    fix(Products): 프로모션 상품 기본 데이터 추가
    - 상품 목록 출력 시, 프로모션 상품이 있지만 기본 상품이 없을 경우 추가로 만드는 기능을 추가했습니다.

--- src/main/java/store/domain/Products.java
+++ src/main/java/store/domain/Products.java
@@ -1,6 +1,8 @@
 package store.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Products {
     List<Product> products;
@@ -15,10 +17,39 @@ public class Products {
                 .toList();
     }
 
-    private List<Product> createProducts(List<List<String>> products) {
-        return products.stream()
-                .map(Product::new)
-                .toList();
+    private List<Product> createProducts(List<List<String>> productsData) {
+        List<Product> productList = new ArrayList<>();
+        for (List<String> productData : productsData) {
+            Product product = new Product(productData);
+            productList.add(product);
+            addProductWithDefault(productsData, productList, productData);
+        }
+        return productList;
+    }
+
+    private void addProductWithDefault(List<List<String>> productsData, List<Product> productList, List<String> productData) {
+        if (!isNotNull(productData)) {
+            return;
+        }
+        Optional<List<String>> optional = findOptionalProduct(productsData, productData);
+        if (optional.isEmpty()) {
+            productList.add(createDefaultProduct(productData));
+        }
+    }
+
+    private boolean isNotNull(List<String> productData) {
+        return !productData.get(3).equals("null");
+    }
+
+    private Optional<List<String>> findOptionalProduct(List<List<String>> productsData, List<String> productData) {
+        return productsData.stream()
+                .filter(e -> e.getFirst().equals(productData.getFirst()))
+                .filter(e -> e.get(3).equals("null"))
+                .findFirst();
+    }
+
+    private Product createDefaultProduct(List<String> productData) {
+        return new Product(List.of(productData.get(0), productData.get(1), "0", "null"));
     }
 
     public List<Product> findProductByEqualsName(String name) {
