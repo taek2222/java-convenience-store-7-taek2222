9822a00d31c262a21a9d86ce60fc26f90dca4039
taek2222 <oot309@daum.net>
2024-11-10 01:38:49 +0900

    test(ProductParserTest): 구매 상품 중복 테스트 추가

--- src/test/java/store/global/util/ProductParserTest.java
+++ src/test/java/store/global/util/ProductParserTest.java
@@ -1,6 +1,6 @@
 package store.global.util;
 
-import java.util.HashMap;
+import java.util.Map;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,11 +12,25 @@ public class ProductParserTest {
         String input = "[사이다-2]";
 
         // when
-        HashMap<String, Integer> result = ProductParser.parseInputProduct(input);
+        Map<String, Integer> result = ProductParser.parseInputProduct(input);
 
         // then
         Assertions.assertThat(result)
                 .containsEntry("사이다", 2)
+                .hasSize(1);
+    }
+
+    @Test
+    void 구매할_상품이_중복될_시_수량을_합산한다() {
+        // given
+        String input = "[사이다-2],[사이다-2]";
+
+        // when
+        Map<String, Integer> result = ProductParser.parseInputProduct(input);
+
+        // then
+        Assertions.assertThat(result)
+                .containsEntry("사이다", 4)
                 .hasSize(1);
     }
 }
