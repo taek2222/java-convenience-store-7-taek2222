5bd9c79cbdb140ea5ec68a366babdb13ad7dfc0f
taek2222 <oot309@daum.net>
2024-11-09 19:07:31 +0900

    feat(Promotions): 프로모션 null 값 공백 변환
    - 파일 읽을 때, null 문자열을 받아올 시 값이 없도록 변경했습니다.
    
    - 상품 목록 출력 기능 완료로 README 기능 목록 체크했습니다.

--- src/main/java/store/domain/product/Promotions.java
+++ src/main/java/store/domain/product/Promotions.java
@@ -2,14 +2,23 @@ package store.domain.product;
 
 public class Promotions {
 
+    private static final String NAME_BLANK = "";
+
     private final String name;
 
     public Promotions(final String name) {
-        this.name = name;
+        this.name = sanitizeName(name);
     }
 
     @Override
     public String toString() {
+        return name;
+    }
+
+    private String sanitizeName(final String name) {
+        if (name.equals("null")) {
+            return NAME_BLANK;
+        }
         return name;
     }
 }
