c76e56353634e43efd9f09f5e1992e398b49874d
taek2222 <oot309@daum.net>
2024-11-10 02:27:24 +0900

    feat(ProductParser): 구매 형식 올바르지 않을 경우 검증
    - 추후 리팩토링을 진행해 가독성을 향상시킬 계획입니다.

--- src/main/java/store/global/util/ProductParser.java
+++ src/main/java/store/global/util/ProductParser.java
@@ -4,21 +4,43 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import store.global.constant.ErrorMessage;
+import store.global.validation.CommonValidator;
 
 public class ProductParser {
 
     public static Map<String, Integer> parseInputProduct(String input) {
-        Pattern pattern = Pattern.compile("\\[(.*?)-(\\d+)]");
+        Pattern pattern = Pattern.compile("^\\[(.*?)-(\\d+)]$");
         Map<String, Integer> product = new HashMap<>();
 
+        Pattern compile = Pattern.compile("^\\[.*]$");
+        Matcher matcher1 = compile.matcher(input);
+        if (!matcher1.find())
+            throw new IllegalArgumentException();
+
         String[] split = input.split(",");
+
         for (String s : split) {
             Matcher matcher = pattern.matcher(s);
-            if (matcher.find()) {
-                String name = matcher.group(1);
-                int quantity = Integer.parseInt(matcher.group(2));
-                product.merge(name, quantity, Integer::sum);
+            System.out.println(s);
+
+            if (!matcher.find() || matcher.groupCount() != 2) {
+                throw new IllegalArgumentException();
             }
+
+            String name = matcher.group(1);
+
+            Pattern compile12 = Pattern.compile("[-\\[\\]]");
+            Matcher matcher12 = compile12.matcher(name);
+
+            if (matcher12.find())
+                throw new IllegalArgumentException();
+            CommonValidator.validateBlank(name, ErrorMessage.INVALID_PRODUCT_ELEMENT);
+
+            int quantity = Integer.parseInt(matcher.group(2));
+
+            System.out.println(name + " " + quantity);
+            product.merge(name, quantity, Integer::sum);
         }
         return product;
     }
