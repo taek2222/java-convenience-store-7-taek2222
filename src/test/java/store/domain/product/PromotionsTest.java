bcfbddea53ec4536e915ea2f56565141ab0a4283
taek2222 <oot309@daum.net>
2024-11-09 18:29:17 +0900

    test(PromotionsTest): 프로모션 null 값 테스트
    - 파일 읽을 때, null 문자열을 인식해 출력할 때도 null 로 출력되는 오류 발견했습니다.
    
    - null인 경우 빈 값을 출력하기 위해 추가 기능을 설계했습니다.

--- src/test/java/store/domain/product/PromotionsTest.java
+++ src/test/java/store/domain/product/PromotionsTest.java
@@ -1,6 +1,7 @@
 package store.domain.product;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 public class PromotionsTest {
@@ -14,7 +15,20 @@ public class PromotionsTest {
         String toString = promotions.toString();
 
         // then
-        Assertions.assertThat(toString)
+        assertThat(toString)
                 .isEqualTo("테스트할인");
+    }
+
+    @Test
+    void 프로모션_null_일_때_빈값을_반환한다() {
+        // given
+        Promotions promotions = new Promotions("null");
+
+        // when
+        String toString = promotions.toString();
+
+        // then
+        assertThat(toString)
+                .isEqualTo("");
     }
 }
