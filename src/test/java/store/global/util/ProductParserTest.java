b45b86a3ebfa783781824e4ee54b891eb243fecd
taek2222 <oot309@daum.net>
2024-11-10 01:48:20 +0900

    test(ProductParserTest): 구매 형식 올바르지 않을 경우 테스트

--- src/test/java/store/global/util/ProductParserTest.java
+++ src/test/java/store/global/util/ProductParserTest.java
@@ -3,6 +3,8 @@ package store.global.util;
 import java.util.Map;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ProductParserTest {
 
@@ -32,5 +34,28 @@ public class ProductParserTest {
         Assertions.assertThat(result)
                 .containsEntry("사이다", 4)
                 .hasSize(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "[사이다-한개]",
+            "[사이다]",
+            "[사이다2]",
+            "[사이다-1],[사이다-2",
+            "사이다-2",
+            "[-2]",
+            "[사이다-]",
+            "[사이다-abc]",
+            "[사이다-2]abc",
+            "[[사이다-2]]",
+            "[123-4]",
+            "[사이다:2]",
+            "[사이다--2]",
+            "[사이다-2][사이다-1]",
+            "[사이다-1],[사이다-2],"
+    })
+    void 구매할_상품의_형식이_올바르지_않을_경우_예외가_발생한다(String input) {
+        Assertions.assertThatThrownBy(() -> ProductParser.parseInputProduct(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
