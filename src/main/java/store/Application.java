36ed7ff5303470662ba9b49377aafce56babbb42
taek2222 <oot309@daum.net>
2024-11-09 18:17:18 +0900

    feat(Application): 프로그램 시작 부분 정의
    - 입력에 파일 읽는 생성자 주입으로 실행 시키도록 전달했습니다.

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,17 @@
 package store;
 
+import store.controller.StoreController;
+import store.view.FileInputView;
+import store.view.InputView;
+import store.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        FileInputView fileInputView = new FileInputView();
+        StoreController storeController = new StoreController(
+                new OutputView(),
+                new InputView(fileInputView)
+        );
+        storeController.run();
     }
 }
