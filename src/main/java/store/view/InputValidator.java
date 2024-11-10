9789c365cc83a4d426c476ba8310a061123cf8c5
taek2222 <oot309@daum.net>
2024-11-11 00:21:26 +0900

    feat(InputValidator): Y|N 검증 추가
    - 입력 과정에 필요한 검증울 추가 선언했습니다.

--- src/main/java/store/view/InputValidator.java
+++ src/main/java/store/view/InputValidator.java
@@ -2,6 +2,7 @@ package store.view;
 
 import static store.global.constant.ErrorMessage.FILE_CONTENT_INSUFFICIENT;
 import static store.global.constant.ErrorMessage.FILE_CONTENT_NULL;
+import static store.global.constant.ErrorMessage.INVALID_INPUT;
 
 import java.util.List;
 import store.global.exception.FileException;
@@ -17,5 +18,10 @@ public class InputValidator {
         if (input.size() <= MINIMUM_VALID_LINES) {
             throw new FileException(FILE_CONTENT_INSUFFICIENT);
         }
+    }
+
+    public static void validateYesOrNoInput(String input) {
+        if (!input.equals("Y") && !input.equals("N"))
+            throw new IllegalArgumentException(INVALID_INPUT.getMessage());
     }
 }
