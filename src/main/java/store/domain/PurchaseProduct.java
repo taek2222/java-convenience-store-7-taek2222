2a40be36128d1e5b816c1c244d94899c5df97269
taek2222 <oot309@daum.net>
2024-11-11 01:03:36 +0900

    feat(PurchaseProduct): 구매 물품 메소드 및 필드 추가
    - 프로모션 계산 과정에 필요한 메소드를 추가 구현했습니다.
    
    - 추후 리팩토링으로 역할을 분리할 계획입니다.

--- src/main/java/store/domain/PurchaseProduct.java
+++ src/main/java/store/domain/PurchaseProduct.java
@@ -3,22 +3,48 @@ package store.domain;
 import static store.global.constant.ErrorMessage.INSUFFICIENT_STOCK;
 
 import java.util.List;
+import java.util.Objects;
 
 public class PurchaseProduct {
-    private final List<Product> purchase;
+    private final List<Product> products;
+    private final int quantity;
 
-    public PurchaseProduct(List<Product> purchase) {
-        this.purchase = purchase;
+    public PurchaseProduct(List<Product> products, int quantity) {
+        this.products = products;
+        validateSufficientStock(quantity);
+        this.quantity = quantity;
     }
 
-    public void validateSufficientStock(int purchaseQuantity) {
-        purchaseQuantity = calculateRemainingStock(purchaseQuantity);
-        if(purchaseQuantity > 0)
+    public String getProductName() {
+        return products.get(0).getProductName();
+    }
+
+    public int calculateRemainingStock() {
+        return products.get(0).calculateRemainingStock(quantity);
+    }
+
+    public int calculatePromotionRate(int remainingStock) {
+        return products.get(0).calculatePromotionRate(quantity - remainingStock);
+    }
+
+    public boolean hasPromotionProduct() {
+        return products.size() == 2;
+    }
+
+    private void validateSufficientStock(int purchaseQuantity) {
+        int remainingStock = calculateTotalRemainingStock(purchaseQuantity);
+        if (remainingStock > 0) {
             throw new IllegalArgumentException(INSUFFICIENT_STOCK.getMessage());
+        }
     }
 
-    private int calculateRemainingStock(int purchaseQuantity) {
-        return purchase.stream()
+    private int calculateTotalRemainingStock(int purchaseQuantity) {
+        return products.stream()
+                .filter(Objects::nonNull)
                 .reduce(purchaseQuantity, (qty, product) -> product.calculateRemainingStock(qty), Integer::sum);
+    }
+
+    public boolean isPromotionAdditionalProduct() {
+        return products.get(0).isPromotionAdditionalProduct(quantity);
     }
 }
