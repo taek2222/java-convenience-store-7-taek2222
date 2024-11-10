ee6d398a6c3de4099afc3299cf15c8f5b0ede3d7
taek2222 <oot309@daum.net>
2024-11-10 19:46:11 +0900

    feat(StoreController): 상점 컨트롤러 기능 추가
    - 목록을 보여주는 로직을 추가했습니다.
    
    - 입력을 받고 처리할 수 있는 parse 로직을 추가했습니다.
    
    - 객체를 선언하는 과정을 추가했습니다.
    
    - 구매 물품을 받아 들이는 로직을 추가했습니다.

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,7 +1,16 @@
 package store.controller;
 
 import java.util.List;
+import java.util.Map;
+import store.domain.Product;
 import store.domain.Products;
+import store.domain.Promotions;
+import store.domain.PromotionsList;
+import store.domain.PurchaseList;
+import store.domain.PurchaseProduct;
+import store.global.util.ProductParser;
+import store.global.util.PromotionParser;
+import store.global.util.PurchaseParser;
 import store.view.InputView;
 import store.view.OutputView;
 
@@ -15,11 +24,58 @@ public class StoreController {
     }
 
     public void run() {
+        displayWelcomeMessage();
+
+        List<List<String>> inputPromotions = inputView.readPromotionsFile();
+        List<Promotions> promotionsList1 = PromotionParser.parseToPromotions(inputPromotions);
+        PromotionsList promotionsList = new PromotionsList(promotionsList1);
+
+        Products products = loadProducts(promotionsList);
+
+        displayProductsInfo(products);
+        handlePurchases(products);
+    }
+
+    private void displayWelcomeMessage() {
         outputView.printWelcomeMessage();
+    }
+
+    private Products loadProducts(PromotionsList promotionsList) {
         List<List<String>> inputProducts = inputView.readProductsFile();
+        List<Product> productList = ProductParser.parseToProducts(inputProducts, promotionsList);
+        return new Products(productList);
+    }
 
-        Products products = new Products(inputProducts);
-
+    private void displayProductsInfo(Products products) {
         outputView.printProductsInfo(products.getAllProductsInfo());
+    }
+
+    private void handlePurchases(Products products) {
+        while (true) {
+            try {
+                Map<String, Integer> purchases = readAndParsePurchaseInput();
+                processPurchases(products, purchases);
+                break;
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private Map<String, Integer> readAndParsePurchaseInput() {
+        String input = inputView.readPurchaseProduct();
+        return PurchaseParser.parseInputProduct(input);
+    }
+
+    private void processPurchases(Products products, Map<String, Integer> purchases) {
+        PurchaseList purchaseList = new PurchaseList();
+
+        for (String productName : purchases.keySet()) {
+            List<Product> foundProducts = products.findProductByEqualsName(productName);
+            PurchaseProduct purchaseProduct = new PurchaseProduct(foundProducts);
+            purchaseProduct.validateSufficientStock(purchases.get(productName));
+
+            purchaseList.addPurchase(purchaseProduct);
+        }
     }
 }
