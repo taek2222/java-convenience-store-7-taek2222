ebf77dc6d60fae1ff4e75e4ab7e537513fb46e97
taek2222 <oot309@daum.net>
2024-11-11 00:22:14 +0900

    feat(InputView): 프로모션 관련 Y|N 응답 추가
    - 재사용에 용이하도록 분리해 사용했습니다. 추후, 멤버십에도 활용할 예정입니다.

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public class InputView {
     private static final String PURCHASE_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    private static final String PROMOTION_NOT_APPLIED_MESSAGE = "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)";
+    private static final String PROMOTION_ADD_MESSAGE = "현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
     private static final String PRODUCTS_FILE_NAME = "products.md";
     private static final String PROMOTIONS_FILE_NAME = "promotions.md";
     private static final int COLUMN_LINE = 1;
@@ -23,6 +25,19 @@ public class InputView {
         return Console.readLine();
     }
 
+    public String readPurchaseWithoutPromotion(String name, int quantity) {
+        return readYesOrNo(String.format(PROMOTION_NOT_APPLIED_MESSAGE, name, quantity));
+    }
+
+    public String readPromotionAddition(String name) {
+        return readYesOrNo(String.format(PROMOTION_ADD_MESSAGE, name));
+    }
+
+    public List<List<String>> readPromotionsFile() {
+        List<List<String>> lists = fileInputView.readFile(PROMOTIONS_FILE_NAME);
+        return lists.subList(COLUMN_LINE, lists.size());
+    }
+
     public List<List<String>> readProductsFile() {
         List<List<String>> inputProducts = fileInputView.readFile(PRODUCTS_FILE_NAME);
         validateNonEmptyFileContents(inputProducts);
@@ -30,8 +45,11 @@ public class InputView {
         return inputProducts.subList(COLUMN_LINE, inputProducts.size()); // 파일 첫 줄의 컬럼을 제외하고 반환
     }
 
-    public List<List<String>> readPromotionsFile() {
-        List<List<String>> lists = fileInputView.readFile(PROMOTIONS_FILE_NAME);
-        return lists.subList(COLUMN_LINE, lists.size());
+    private String readYesOrNo(String message) {
+        System.out.println(NEW_LINE + message);
+        String readLine = Console.readLine().toUpperCase();
+        InputValidator.validateYesOrNoInput(readLine);
+
+        return readLine;
     }
 }
