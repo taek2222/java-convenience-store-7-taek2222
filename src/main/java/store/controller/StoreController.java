4e06ede04448e2e43a16c625a43079927b1fd0ac
taek2222 <oot309@daum.net>
2024-11-11 04:30:53 +0900

    feat(StoreController): 컨트롤러 기능 구현
    - 프로모션 및 질의 응답 과정 기능까지 구현 완료했습니다.
    
    - 리팩토링이 급해, 빠르게 시도 해 보겠습니다.

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,12 +1,15 @@
 package store.controller;
 
+import camp.nextstep.edu.missionutils.DateTimes;
+import java.time.LocalDateTime;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import store.domain.PaymentProductList;
 import store.domain.Product;
 import store.domain.Products;
 import store.domain.Promotions;
 import store.domain.PromotionsList;
-import store.domain.PurchaseList;
 import store.domain.PurchaseProduct;
 import store.global.util.ProductParser;
 import store.global.util.PromotionParser;
@@ -26,18 +29,72 @@ public class StoreController {
     public void run() {
         displayWelcomeMessage();
 
-        List<List<String>> inputPromotions = inputView.readPromotionsFile();
-        List<Promotions> promotionsList1 = PromotionParser.parseToPromotions(inputPromotions);
-        PromotionsList promotionsList = new PromotionsList(promotionsList1);
-
+        PromotionsList promotionsList = loadPromotions();
         Products products = loadProducts(promotionsList);
 
         displayProductsInfo(products);
-        handlePurchases(products);
+        List<PurchaseProduct> purchases = handlePurchases(products);
+
+        PaymentProductList paymentProductList = new PaymentProductList();
+        LocalDateTime now = DateTimes.now();
+
+        for (PurchaseProduct purchase : purchases) {
+            if (purchase.hasPromotionProduct() && purchase.nodatelate(now)) {
+                int calculate = purchase.calculate();
+                int remainingStock = purchase.calculateRemainingStock();
+                if (remainingStock > 0) {
+                    int promotionRate = purchase.calculatePromotionRate(Math.abs(remainingStock));
+                    int quantity = remainingStock + promotionRate;
+                    outputView.printPromotionNotApplied(purchase.getProductName(),
+                            quantity);
+                    boolean answer = inputView.readYesOrNo();
+
+                    if (!answer) {
+                        purchase.decrease(quantity);
+                    }
+
+                    paymentProductList.addPaymentProduct(purchase.createPaymentProduct(0));
+                    continue;
+                }
+
+                if (remainingStock != 0) {
+                    outputView.printPromotionAddition(purchase.getProductName());
+                    boolean answer = inputView.readYesOrNo();
+                    purchase.calculatePromotionRate(0);
+                    int calculate1 = purchase.calculate();
+                    if (answer) {
+                        calculate1 += 1;
+                    }
+                    paymentProductList.addPaymentProduct(purchase.createPaymentProduct(calculate1));
+                    continue;
+                }
+                paymentProductList.addPaymentProduct(purchase.createPaymentProduct(calculate));
+                continue;
+            }
+            paymentProductList.addPaymentProduct(purchase.createPaymentProduct(0));
+        }
+
+        List<String> infos = paymentProductList.infoProduct();
+        List<String> strings = paymentProductList.infoPromotion();
+        boolean answer = inputView.readMemberShip();
+        if (answer) {
+            paymentProductList.membership();
+        }
+        List<String> result = paymentProductList.finalResult();
+
+        outputView.printPaymentInfoResult(infos);
+        outputView.printPromotionInfoResult(strings);
+        outputView.printFinalResult(result);
     }
 
     private void displayWelcomeMessage() {
         outputView.printWelcomeMessage();
+    }
+
+    private PromotionsList loadPromotions() {
+        List<List<String>> inputPromotions = inputView.readPromotionsFile();
+        List<Promotions> promotionsList1 = PromotionParser.parseToPromotions(inputPromotions);
+        return new PromotionsList(promotionsList1);
     }
 
     private Products loadProducts(PromotionsList promotionsList) {
@@ -50,12 +107,11 @@ public class StoreController {
         outputView.printProductsInfo(products.getAllProductsInfo());
     }
 
-    private void handlePurchases(Products products) {
+    private List<PurchaseProduct> handlePurchases(Products products) {
         while (true) {
             try {
                 Map<String, Integer> purchases = readAndParsePurchaseInput();
-                processPurchases(products, purchases);
-                break;
+                return processPurchases(products, purchases);
             } catch (IllegalArgumentException e) {
                 outputView.printErrorMessage(e.getMessage());
             }
@@ -67,15 +123,13 @@ public class StoreController {
         return PurchaseParser.parseInputProduct(input);
     }
 
-    private void processPurchases(Products products, Map<String, Integer> purchases) {
-        PurchaseList purchaseList = new PurchaseList();
-
+    private List<PurchaseProduct> processPurchases(Products products, Map<String, Integer> purchases) {
+        LinkedList<PurchaseProduct> purchaseProducts = new LinkedList<>();
         for (String productName : purchases.keySet()) {
             List<Product> foundProducts = products.findProductByEqualsName(productName);
-            PurchaseProduct purchaseProduct = new PurchaseProduct(foundProducts);
-            purchaseProduct.validateSufficientStock(purchases.get(productName));
-
-            purchaseList.addPurchase(purchaseProduct);
+            PurchaseProduct purchaseProduct = new PurchaseProduct(foundProducts, purchases.get(productName));
+            purchaseProducts.add(purchaseProduct);
         }
+        return purchaseProducts;
     }
 }
