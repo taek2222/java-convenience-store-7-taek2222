9eebb8d613a1e62812b3daa2be76a8869d97f92a
taek2222 <oot309@daum.net>
2024-11-11 23:43:39 +0900

    remove(CommonValidator): 사용X 메서드 삭제
    - 사용하지 않는 메서드를 삭제했습니다.

--- src/main/java/store/global/validation/CommonValidator.java
+++ src/main/java/store/global/validation/CommonValidator.java
@@ -4,12 +4,6 @@ import store.global.constant.ErrorMessage;
 
 public class CommonValidator {
 
-    public static void validateNull(final String input, final ErrorMessage error) {
-        if (input == null) {
-            throw new IllegalArgumentException(error.getMessage());
-        }
-    }
-
     public static void validateBlank(final String input, final ErrorMessage error) {
         if (input.isBlank()) {
             throw new IllegalArgumentException(error.getMessage());
